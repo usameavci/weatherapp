819a83f4edde3dc7ddadb648095c9440eb827870
Üsame Fethullah AVCI <usameavci@gmail.com>
2018-09-30 23:44:46 +0300

    log resource id field added

--- api/app/Http/Resources/Log.php
+++ api/app/Http/Resources/Log.php
@@ -17,6 +17,7 @@ class Log extends JsonResource
         $isIndexRoute = $request->route()->uri() != 'api/logs';
 
         return [
+            'id' => $this->id,
             'user' => new User($this->user),
             'place' => new Place($this->place),
             'executed_at' => $this->executed_at,
